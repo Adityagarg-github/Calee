7603bc62f42b41c4874e03ac6578b75940ad45a3
aksahil2205 <120297640+aksahil2205@users.noreply.github.com>
2025-03-14 02:31:06 +0530

    Added swipe gesture
    Added the swipe gesture so user can swipe between pages and updated the bottom navigation bar UI.

--- lib/views/PBTabView.dart
+++ lib/views/PBTabView.dart
@@ -11,22 +11,37 @@ import 'package:iitropar/views/groups.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'homePage/admin_page.dart';
 
-class MainLandingPage extends StatelessWidget {
+class MainLandingPage extends StatefulWidget {
   const MainLandingPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return PersistentTabView(
-      context,
-      screens: _buildScreens(),
-      items: _navbarItems(),
-      navBarStyle: NavBarStyle.style9,
-      backgroundColor: Color(secondaryLight),
-      screenTransitionAnimation: const ScreenTransitionAnimation(
-          animateTabTransition: false,
-          duration: Duration(seconds: 1),
-          curve: Curves.ease),
-    );
+  _MainLandingPageState createState() => _MainLandingPageState();
+}
+
+class _MainLandingPageState extends State<MainLandingPage> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    _pageController.jumpToPage(index);
   }
 
   List<Widget> _buildScreens() {
@@ -36,129 +51,98 @@ class MainLandingPage extends StatelessWidget {
         const EventCalendarScreen(appBarBackgroundColor: Color(0xFF0D47A1)),
         const Events(appBarBackgroundColor: Color(0xFF0D47A1)),
         const MessMenuPage(appBarBackgroundColor: Color(0xFF0D47A1)),
-        const QuickLinks(appBarBackgroundColor: Color(0xFF0D47A1))
+        const QuickLinks(appBarBackgroundColor: Color(0xFF0D47A1)),
       ];
-    }
-    else if (Ids.role == "faculty") {
+    } else if (Ids.role == "faculty" || Ids.role == "club") {
       return [
         const HomePage(),
         const EventCalendarScreen(appBarBackgroundColor: Color(0xFFAD1457)),
         const Events(appBarBackgroundColor: Color(0xFFAD1457)),
         const MessMenuPage(appBarBackgroundColor: Color(0xFFAD1457)),
-        const QuickLinks(appBarBackgroundColor: Color(0xFFAD1457))
+        const QuickLinks(appBarBackgroundColor: Color(0xFFAD1457)),
       ];
-    }
-    else if (Ids.role == "club") {
-      return [
-        const HomePage(),
-        const EventCalendarScreen(appBarBackgroundColor: Color(0xFF32A83C)),
-        const Events(appBarBackgroundColor: Color(0xFF32A83C)),
-        const MessMenuPage(appBarBackgroundColor: Color(0xFF32A83C)),
-        const QuickLinks(appBarBackgroundColor: Color(0xFF32A83C))
-      ];
-    }
-    else {
+    } else {
       return [
         const HomePage(),
         const EventCalendarScreen(appBarBackgroundColor: Color(0xFF42A5F5)),
         const Events(appBarBackgroundColor: Color(0xFF42A5F5)),
         const Groups(),
         const MessMenuPage(appBarBackgroundColor: Color(0xFF42A5F5)),
-        const QuickLinks(appBarBackgroundColor: Color(0xFF42A5F5))
+        const QuickLinks(appBarBackgroundColor: Color(0xFF42A5F5)),
       ];
     }
   }
 
-  List<PersistentBottomNavBarItem> _navbarItems() {
-    if(Ids.role == "admin") {
-      return [
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.home),
-            title: 'Home',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.calendar),
-            title: 'Calendar',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(Icons.event),
-            title: 'Events',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(Icons.food_bank_rounded),
-            title: 'Mess Menu',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.link),
-            title: 'Quick Links',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-      ];
-    }
-    else if(Ids.role == "faculty" || Ids.role == "club") {
-      return [
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.home),
-            title: 'Home',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.calendar),
-            title: 'Calendar',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(Icons.event),
-            title: 'Events',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(Icons.food_bank_rounded),
-            title: 'Mess Menu',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-        PersistentBottomNavBarItem(
-            icon: const Icon(CupertinoIcons.link),
-            title: 'Quick Links',
-            activeColorPrimary: Color(primaryLight),
-            inactiveColorPrimary: Colors.grey[400]),
-      ];
-    }
+  List<BottomNavigationBarItem> _navbarItems() {
     return [
-      PersistentBottomNavBarItem(
-          icon: const Icon(CupertinoIcons.home),
-          title: 'Home',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
-      PersistentBottomNavBarItem(
-          icon: const Icon(CupertinoIcons.calendar),
-          title: 'Calendar',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
-      PersistentBottomNavBarItem(
-          icon: const Icon(Icons.event),
-          title: 'Events',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
-      PersistentBottomNavBarItem(
-          icon: const Icon(Icons.group),
-          title: 'Groups',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
-      PersistentBottomNavBarItem(
-          icon: const Icon(Icons.food_bank_rounded),
-          title: 'Mess Menu',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
-      PersistentBottomNavBarItem(
-          icon: const Icon(CupertinoIcons.link),
-          title: 'Quick Links',
-          activeColorPrimary: Color(primaryLight),
-          inactiveColorPrimary: Colors.grey[400]),
+      const BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.home),
+        label: 'Home',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.calendar),
+        label: 'Calendar',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.event),
+        label: 'Events',
+      ),
+      if (Ids.role != "faculty" && Ids.role != "admin")
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.group),
+          label: 'Groups',
+        ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.food_bank_rounded),
+        label: 'Mess Menu',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.link),
+        label: 'Quick Links',
+      ),
     ];
   }
+
+  // @override
+  // void dispose() {
+  //   _pageController.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(secondaryLight),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: _buildScreens(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _navbarItems(),
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: _getSelectedItemColor(),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+            _pageController.jumpToPage(index);
+          });
+        },
+      ),
+    );
+  }
+
+  Color _getSelectedItemColor() {
+    if (Ids.role == "admin") {
+      return const Color(0xFF0D47A1); // Dark Blue
+    } else if (Ids.role == "faculty") {
+      return const Color(0xFFAD1457); // Pink
+    } else if (Ids.role == "club") {
+      return Colors.green;
+    } else {
+      return Colors.blue;
+    }
+  }
+
 }
